dfab0f627a099bc3c65c22e031500cd6d4253302
Will-Andrade <willian.santos.andra@outlook.com>
2023-11-27 01:05:16 -0400

    feat: switched bubble sort to merge sort

--- recessQueue.ts
+++ recessQueue.ts
@@ -1,19 +1,35 @@
-const orderGrades = (arrInput: number[]): number[] => {
-  let arrCopy = arrInput.map(num => num);
+const mergeSort = (arr: number[]): number[] => {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-  for (let i = 0; i < arrCopy.length - 1; i++) {
-    for (let j = 0; j < arrCopy.length - 1 - i; j++) {
-      if (arrCopy[j] < arrCopy[j + 1]) {
-        [arrCopy[j], arrCopy[j + 1]] = [arrCopy[j + 1], arrCopy[j]];
-      }
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left: number[], right: number[]): number[] => {
+  let result: number[] = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] >= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
     }
   }
 
-  return arrCopy;
+  return result.concat(left.slice(i)).concat(right.slice(j));
 };
 
 const recessQueue = (N: number, M: number[]): number => {
-  let orderedGrades: number[] = orderGrades(M);
+  let orderedGrades: number[] = mergeSort(M);
 
   const result = orderedGrades.reduce((acc: number, orderedGrade: number, index: number): number => {
     if (orderedGrade === M[index]) {
